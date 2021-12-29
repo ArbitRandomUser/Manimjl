2408aba4539fa774e6c7bbd0d2de64a960245115
arbitrandomuser <arbitrandomuser@gmail.com>
2021-12-30 04:21:44 +0530

    fade in tested

--- manim.jl
+++ manim.jl
@@ -1,33 +1,97 @@
 using Luxor
+using LinearAlgebra
 using FileIO,ImageMagick,Colors,FixedPointNumbers,VideoIO
 res = [640,480]
-#canvas = Drawing(res...,"/tmp/drawing.svg")
-jlc(x) = @imagematrix juliacircles(x) res[1]  res[2] 
-#jlc(100)
-#save("test2.png",ans)
-#b = zeros(ARGB32,res[2],res[1],100)
-frames = Array{ Matrix{RGB{N0f8}} }([])
 const framerate = 30
-#save("test.mp4",frames,framerate=30)
-#jlcfix(i) = Matrix{RGB{N0f8}}(jlc(i))
-#
+Luxor.Point(x::Array) = Luxor.Point(Tuple(x[1:2]))
+
 abstract type Object
 end
 
-struct Circle<:Object
+mutable struct Arc<:Object
   centre::Array
   radius::Float64
-  visible::Float64
+  angle1::Real
+  angle2::Real
+  opacity::Float64
+  misc::Dict
+end
+#constructors
+Arc(o::Array,r::Float64,a1::Real,a2::Real) = Arc(o,r,a1,a2,0,Dict([]))
+Circle(o::Array,r::Float64) = Arc(o,r,0,2π)
+
+function drawobject(o::Arc)
+  setopacity(o.opacity)
+  Luxor.arc(o.centre[1],o.centre[2],o.radius,o.angle1,o.angle2,:stroke) 
+  setopacity(0)
 end
 
-O = [0,0,0]
-UP = [0,1,0]
-DOWN = [0,-1,0]
-LEFT = [-1,0,0]
-RIGHT = [1,0,0]
+mutable struct Line<:Object
+  beginp::Array
+  endp::Array
+  centre::Array
+  opacity::Float64
+  misc::Dict
+end
+#constructors
+Line(b::Array,e::Array) = Line(b,e,(b.-e)/2, 0,Dict([]))
 
-objects = Array{Object}([Circle(O,1),Circle(O.+UP,1)])
+function drawobject(o::Line;part=1.0)
+  setopacity(o.opacity)
+  Luxor.line(Point(o.beginp),Point(o.endp),:stroke)
+  setopacity(0)
+end
+
+mutable struct Polygon<:Object
+  points::Array{Array}
+end
+
+O = [0.0,0,0]
+UP = [0.0,1,0]
+DOWN = [0.0,-1,0]
+LEFT = [-1.0,0,0]
+RIGHT = [1.0,0,0]
+
+function FadeInObject(o::Object,t=1)
+  nframes = floor(t*framerate)
+  interpol(t) = t
+  for (i,t) in enumerate(range(0,t,length=nframes))  
+    o.opacity = interpol(t) 
+    push!(frames,drawframe())
+  end
+end
 
 function CreateObject(o::Object,t=1)
-  Δt = t/framerate
+  nframes -= floor(t*framerate)
+  for (i,t) in enumerate(range(0,t,length=nframes))
+    
+    push!(frames,drawframe())
+  end
 end
+
+function drawframe()
+  img = Drawing(res...,:image)
+  origin()
+  sethue("white")
+  for o in objects
+    drawobject(o)
+  end
+  return image_as_matrix() 
+end
+
+objects = Array{Object}([Circle(O,10.0),Circle(O.+50*UP,10.0),Line(O,20*UP)])
+img = drawframe()
+save("test.png",Array{RGB{N0f8}}(img))
+
+frames = Array{ Matrix{RGB{N0f8}} }([])
+FadeInObject(objects[1],2)
+FadeInObject(objects[3],2)
+save("test.mp4",frames,framerate=30)
+run(`mpv test.mp4`)
+
+#a = Drawing(500,500,:image)
+#origin()
+#setcolor("red")
+#circle(Point(0,1),100,:fill)
+#mat = image_as_matrix()
+#finish()
