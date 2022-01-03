bafacd9e984b0507796bdf72b19798c75825249f
John George Francis <arbitrandomuser@gmail.com>
2022-01-04 02:06:24 +0530

    changed Float64 to Real

--- objects.jl
+++ objects.jl
@@ -1,19 +1,19 @@
 #Arc and Circle
 mutable struct Arc <: Object
   centre::Array
-  radius::Float64
+  radius::Real
   angle1::Real
   angle2::Real
-  opacity::Float64
+  opacity::Real
   transform::Function
   misc::Dict
 end
 #constructors
-Arc(o::Array, r::Float64, a1::Real, a2::Real) = Arc(o, r, a1, a2, 0, x -> x, Dict([]))
-Arc(o::Array, r::Float64, a1::Real, a2::Real, op::Float64) =
+Arc(o::Array, r::Real, a1::Real, a2::Real) = Arc(o, r, a1, a2, 0, x -> x, Dict([]))
+Arc(o::Array, r::Real, a1::Real, a2::Real, op::Real) =
   Arc(o, r, a1, a2, op, x -> x, Dict([]))
-Circle(o::Array, r::Float64) = Arc(o, r, 0, 2π)
-Circle(o::Array, r::Float64, op::Float64) = Arc(o, r, 0, 2π, op)
+Circle(o::Array, r::Real) = Arc(o, r, 0, 2π)
+Circle(o::Array, r::Real, op::Real) = Arc(o, r, 0, 2π, op)
 
 function drawobject(o::Arc)
   setopacity(o.opacity)
@@ -29,13 +29,13 @@ end
 mutable struct Line <: Object
   beginp::Array
   endp::Array
-  opacity::Float64
+  opacity::Real
   transform::Function
   misc::Dict
 end
 #constructors
 Line(b::Array, e::Array) = Line(b, e, 0.0, x -> x, Dict([]))
-Line(b::Array, e::Array, op::Float64) = Line(b, e, op, x -> x, Dict([]))
+Line(b::Array, e::Array, op::Real) = Line(b, e, op, x -> x, Dict([]))
 
 function drawobject(o::Line; part = 1.0)
   setopacity(o.opacity)
