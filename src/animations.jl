909bbf55bc82ef8dce7c2314bae303f81479d41d
John George Francis <arbitrandomuser@gmail.com>
2022-01-14 16:46:27 +0530

    long boring text describing Play , also CreateObject adds object to Scene if not in Scene already

--- src/animations.jl
+++ src/animations.jl
@@ -7,7 +7,15 @@ function SetOpacity(o::Object,op::Float64, T=1)#::Base.Generator{Vector{Function
 end
 function CreateObjectPartial(o::Object,p::Real, T::Real = 1; easefn = easeinoutcubic)#::Array{Function}
   #draws p fraction of the object
+  """Creates the object partially, 
+  `o` is the object
+  `p` is the fraction of which it should be drawn
+  `T` is the Time taken to create the object
+  """
   @assert 0<=p<=1
+  if o not in Scene
+    push!(Scene,o)
+  end
   nframes = floor(T * framerate)
   t_index = length(o.transforms)
   push!(o.transforms, x->x ) #push a new transform
@@ -15,6 +23,7 @@ function CreateObjectPartial(o::Object,p::Real, T::Real = 1; easefn = easeinoutc
   ret
 end
 function UncreateObjectPartial(o::Object,p::Real, T::Real = 1; easefn = easeinoutcubic)
+  """Uncreates the object ,""" 
   #undraws p fraction of the object
   @assert 0<=p<=1
   nframes = floor(T * framerate)
@@ -75,9 +84,30 @@ function Wait(T::Real = 1.0;)
 end
 
 function Play(actionlists...)
-  # actions is a list like 
-  # [ [f1,f2,f3..]  , [g1,g2,g3...],... ]
-  # where fi,gi are all functions 
+  """
+  usage:
+  `Play( Animation1(...) , Animation2(...))`
+  where `Animation` is one of the animation functions.
+  every Animation is played simultaneusly, if you want 
+  Animation1 , to be played while Animation2 and Animation3 are played in
+  sequence use the Seq function
+
+  `Play( Animation1(...) , Seq( Animation2(...) , Animation3(...)))`
+
+  actionlists is an array of actionlist.
+  actionlist is an array(or iterable) of functions,
+  typically returned by the animation functions.
+  something like [ [f1,f2,f3...] , [g1,g2,g3...], [h1,h2,h3... ]
+  where fi and gi are functions.
+  
+
+  every action in an actionlist is argless function 
+  the function typically modifies objects properties
+  Play then runs f1,g1,h1... which modify the objects
+  then a frame is drawn. Next it runs f2,g2,h2.... which modify
+  the objects for the next frame .. and so on. Thus animations are made
+
+  """
   nframes = maximum(length.(actionlists))
   i=0 #state of generator
   while (i < nframes)
